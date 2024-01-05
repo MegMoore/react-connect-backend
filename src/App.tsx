859e3969e5980932b8f8434e977690e19d3c66ac
MegMoore <136514941+MegMoore@users.noreply.github.com>
2024-01-05 12:18:20 -0500

    I got the information to display!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
-import ProductList from "./components/ProductList";
 import axios from "axios";
 import FosterKids from "./components/FosterKids";
 
-
-
 function App() {
-
-  
- // const [category, setCatagory] = useState("");
+  const [fosterKids, setFosterKids] = useState([]);
 
   return (
     <div>
-     <FosterKids 
-     
-     />
+      {/* Pass fosterKids as a prop */}
+      <FosterKids fosterKids={fosterKids} />
     </div>
-    // <div>
-    //   <select
-    //     className="form-select"
-    //     onChange={(event) => setCatagory(event.target.value)}
-    //   >
-    //     <option value=""></option>
-    //     <option value="Clothing">Clothing</option>
-    //     <option value="Household">Household</option>
-    //   </select>
-    //   <ProductList category={category}></ProductList>
-    // </div>
-  )
-  }
+  );
+}
 
 export default App;
--- src/components/FosterKids.tsx
+++ src/components/FosterKids.tsx
@@ -1,28 +1,43 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-
-
-function FosterKids() {
-    const [fosterKids, setFosterKids] = useState<fosterKids[]>([]);
-  
-    useEffect(() =>{
-        axios
-      .get<fosterKids[]>("https://localhost:7206/api/fosterKids")
-      .then((res) => console.log(res.data))
-      .catch((error) => console.error("Error", error));
-  });
-    useEffect((fosterKids) => {
-        console.log("Information", fosterKids);
-       
-        
-    })
-    
-    return (
-       <div>
-        Foster Kids
-       </div>
-  )
+interface FosterKid {
+  id: number;
+  firstName: string;
+  sacwis: string;
+  // Add other properties as needed
 }
 
-export default FosterKids
+interface FosterKidsProps {
+  fosterKids: FosterKid[];
+}
+
+function FosterKids({ fosterKids: initialFosterKids }: FosterKidsProps) {
+  const [fosterKids, setFosterKids] = useState<FosterKid[]>(initialFosterKids);
+
+  useEffect(() => {
+    axios
+      .get<FosterKid[]>("https://localhost:7206/api/fosterKids")
+      .then((res) => {
+        setFosterKids(res.data);
+        console.log("Information", res.data);
+      })
+      .catch((error) => console.error("Error", error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Foster Kids</h2>
+      {fosterKids.map((kid) => (
+        <div key={kid.id}>
+          <p>ID: {kid.id}</p>
+          <p>First Name: {kid.firstName}</p>
+          <p>Sacwis: {kid.sacwis}</p>
+          {/* Add more properties as needed */}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FosterKids;
